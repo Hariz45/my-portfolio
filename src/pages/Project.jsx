3f817a3d5c86f209c6403b0ca42a4c89a356f06c
harihifihost <hariprasaths@hifihost.com>
2026-07-02 15:52:05 +0530

    add a new project container

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import rsw from "../assets/rsw.png"
 import cr from "../assets/cr.png"
 import mart from "../assets/mart.svg"
+import Tvk from "../assets/tvk.png"
 
 
 const Project = () => {
@@ -11,15 +12,16 @@ const Project = () => {
       <div className='project-cont'>
 
         <div className='card-container'>
-          <img src={mart} alt="R-Mart Project" width={300}  height={300} />
+          <img src={Tvk}alt="company website" width={300}  height={300} />
           <div className='card-content'>
-            <h3>R-Mart</h3>
-            <p className='para-tag' >R-Mart is a Ecommerce website . it was build with react.js.the project features a modern UI, responsive design, and seamless user experience.</p>
-            <a href="https://my-mart-gilt.vercel.app/" target="_blank">
+            <h3>TVK People's Grievance</h3>
+            <p className='para-tag' > Developed a responsive business website for a People complaining their area issues via this website. The website showcases peoples can able to raise their constituency problems share to their MLA.it was developed only in front end using react js. </p>
+            <a href="https://tvk-web-topaz.vercel.app/" target="_blank">
               <button className='p-btn'>View Live</button>
             </a>
           </div>
         </div>
+        
         <div className='card-container'>
           <img src="/food.png" alt="Ssc Food Order Project" width={300} height={300} />
           <div className='card-content'>
@@ -49,6 +51,18 @@ const Project = () => {
             <h3>CR Build Service</h3>
             <p className='para-tag' > Developed a responsive business website for a cleaning service company. The website showcases commercial and residential cleaning services, company information, contact details, and service offerings with a modern UI and mobile-friendly design. </p>
             <a href="https://cr-build-service.vercel.app/" target="_blank">
+              <button className='p-btn'>View Live</button>
+            </a>
+          </div>
+        </div>
+
+
+        <div className='card-container'>
+          <img src={mart} alt="R-Mart Project" width={300}  height={300} />
+          <div className='card-content'>
+            <h3>R-Mart</h3>
+            <p className='para-tag' >R-Mart is a Ecommerce website . it was build with react.js.the project features a modern UI, responsive design, and seamless user experience.</p>
+            <a href="https://my-mart-gilt.vercel.app/" target="_blank">
               <button className='p-btn'>View Live</button>
             </a>
           </div>
